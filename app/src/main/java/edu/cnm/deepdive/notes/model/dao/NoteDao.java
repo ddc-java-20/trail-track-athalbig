f862e3f54ded6b888756fbb4f85e0bccf991aa94
Aaron Halbig <aaronhalbig@gmail.com>
2025-02-10 15:24:29 -0700

    Note DAO completed (for now)

--- app/src/main/java/edu/cnm/deepdive/notes/model/dao/NoteDao.java
+++ app/src/main/java/edu/cnm/deepdive/notes/model/dao/NoteDao.java
@@ -2,10 +2,16 @@ package edu.cnm.deepdive.notes.model.dao;
 
 import static androidx.lifecycle.Transformations.map;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
 import edu.cnm.deepdive.notes.model.entity.Note;
+import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Single;
+import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
 
@@ -28,4 +34,43 @@ public interface NoteDao {
 
   @Insert
   Single<List<Long>> insert(Note... notes);
+
+  @Update
+  Completable update(Note note);
+
+  @Update
+  Completable update(Collection<Note> notes);
+
+  @Update
+  Completable update(Note... note);
+
+  @Delete
+  Completable delete(Note note);
+
+  @Delete
+  Completable delete(Collection<Note> notes);
+
+  @Delete
+  Completable delete(Note... note);
+
+  @Query("SELECT * FROM note ORDER BY created_on ASC")
+  LiveData<List<Note>> selectByCreatedOnAsc();
+
+  @Query("SELECT * FROM note ORDER BY created_on DESC")
+  LiveData<List<Note>> selectByCreatedOnDesc();
+
+  @Query("SELECT * FROM note WHERE created_on >= :rangeStart AND created_on < :rangeEnd  ORDER BY created_on ASC")
+  LiveData<List<Note>> selectWhereCreatedOnInRangeByCreatedOnAsc(Instant rangeStart, Instant rangeEnd);
+
+  @Query("SELECT * FROM note ORDER BY title ASC")
+  LiveData<List<Note>> selectByTitleAsc();
+
+  @Query("SELECT * FROM note ORDER BY title DESC")
+  LiveData<List<Note>> selectByTitleDesc();
+
+  @Query("SELECT * FROM note WHERE title LIKE :filter ORDER BY title ASC")
+  LiveData<List<Note>> selectWhereTitleLikeByTitleAsc(String filter);
+
+
+
 }
