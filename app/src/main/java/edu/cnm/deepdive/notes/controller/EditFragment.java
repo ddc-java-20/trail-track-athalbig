0952aed4dbefc18e6024c96c20e1c5af7a84304b
Aaron Halbig <aaronhalbig@gmail.com>
2025-02-18 11:06:26 -0700

    EditFragment in progress (layout in progress).

--- app/src/main/java/edu/cnm/deepdive/notes/controller/EditFragment.java
+++ app/src/main/java/edu/cnm/deepdive/notes/controller/EditFragment.java
@@ -2,19 +2,33 @@ package edu.cnm.deepdive.notes.controller;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.notes.R;
 
+@AndroidEntryPoint
 public class EditFragment extends BottomSheetDialogFragment {
+
+  private static final String TAG = EditFragment.class.getSimpleName();
+
+  @ColorInt
+  private int cancelColor;
+  @ColorInt
+  private int saveColor;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     // TODO: 2/18/25 Read any input arguments.
+    cancelColor = getThemeColor(R.attr.colorCancel);
+    saveColor = getThemeColor((R.attr.colorSave))
   }
 
   @NonNull
@@ -43,4 +57,12 @@ public class EditFragment extends BottomSheetDialogFragment {
     // TODO: 2/18/25 Set binding reference(s) to null.
     super.onDestroyView();
   }
+
+  @ColorInt
+  private int getThemeColor(int colorAttr) {
+    TypedValue typedValue = new TypedValue();
+    requireContext().getTheme().resolveAttribute(colorAttr, typedValue, true);
+    return typedValue.data;
+  }
+
 }
